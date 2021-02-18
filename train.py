e842868da579064c3b34f69bde9c92d8fcdf1dbd
Daniel <daniel.azevedo@nilg.ai>
2021-02-18 08:23:52 +0000

    changes

--- train.py
+++ train.py
@@ -12,8 +12,8 @@ y_train = np.genfromtxt("data/train_labels.csv")
 
 
 # Fit a model
-clf = RandomForestClassifier()
-# clf = SVC()
+# clf = RandomForestClassifier()
+clf = SVC()
 clf.fit(X_train, y_train)
 print("Training model...")
 with open('model.pickle', 'wb') as f:
